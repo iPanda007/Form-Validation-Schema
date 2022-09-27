bb749933215f1e705b9bc223d832af1f7b1a03c2
iPanda007 <aungkyawkhaing2000@gmail.com>
2022-09-27 16:46:44 +0630

    form valigation schema

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import {set, useForm} from "react-hook-form"
+import {yupResolver} from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-function App() {
+
+
+const App = () => {
+
+
+  const [confirm,setConfirm] = useState("");
+
+const schema = yup.object().shape({
+  firstName: yup.string().required("need name"),
+  lastName:yup.string().required() ,
+  email: yup.string().required() ,
+  age: yup.number().positive().integer().required() ,
+  password: yup.string().min(4).max(15).required(),
+  confirmPassword: yup.string().required('Confirm Password is required')
+  .oneOf([yup.ref("password"),null],'Password Does not match')
+})
+  const {register,handleSubmit,formState:{ errors }} = useForm({
+    resolver:yupResolver(schema)
+  })
+
+  const submitForm = (data)=>{
+       console.log(data)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <form onSubmit={handleSubmit(submitForm)}>
+           <input type="text"  {...register("firstName",{ required: true })} name="firstName" placeholder="first Name..."/>
+           <p>{errors.firstName?.message}</p>
+           <input type="text" {...register("lastName",{ required: true })} name="lastName"  placeholder='last Name...' />
+            <p>{errors.lastName?.message}</p>
+           <input type="text" {...register("email",{ required: true })} name="email"  placeholder='Email...'/>
+            <p>{errors.email?.message}</p>
+            <input type="text" {...register("age",{ required: true })} name="age"  placeholder='age...'/>
+            <p>{errors.age?.message}</p>
+           <input type="text" {...register("password",{ required: true})} name="password"  placeholder="Password..."/>
+              <p>{errors.password?.message}</p>
+           <input type="text" {...register("confirmPassword",{ required: true})}  name="confirm password" placeholder='Password...' />
+           <p>{errors.confirmPassword?.message}</p>
+           <input type="submit" />
+
+       </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
